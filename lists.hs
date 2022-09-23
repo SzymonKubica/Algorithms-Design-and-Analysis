33a5be1c73fcaaea94b723f1cba86b3aea26d1b1
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-23 23:10:00 +0200

    Added the implementation of the dequeue.

--- lists.hs
+++ lists.hs
@@ -5,11 +5,11 @@ data DList a = DList ([a] -> [a])
 instance Show a => Show (DList a) where
   show (DList fxs) = show (fxs [])
 
-toList:: DList a -> [a]
+toList :: DList a -> [a]
 toList (DList fxs) = fxs []
 
-fromList:: [a] -> DList a
-fromList xs = DList (xs++)
+fromList :: [a] -> DList a
+fromList xs = DList (xs ++)
 
 concatD :: DList a -> DList a -> DList a
 concatD (DList fxs) (DList fys) = DList (fxs . fys)
@@ -19,11 +19,10 @@ concat' xss = toList (foldr concatD (DList id) (map fromList xss))
 
 class List a where
   empty :: List a
-  cons  :: a -> List a -> List a
-  snoc  :: List a -> a -> List a
-  head  :: List a -> a
-  tail  :: List a -> List a
-  (++)  :: List a -> List a -> List a
+  cons :: a -> List a -> List a
+  snoc :: List a -> a -> List a
+  head :: List a -> a
+  tail :: List a -> List a
+  (++) :: List a -> List a -> List a
   toList :: List a -> [a]
   fromList :: [a] -> List a
-
