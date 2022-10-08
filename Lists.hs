b4fcc1b42596dcca06eea978bdc0fc6a75853302
SzymonKubica <szymo.kubica@gmail.com>
2022-10-08 13:24:31 +0100

    Partially fixed the implementation of the List class definition.

--- Lists.hs
+++ Lists.hs
@@ -1,15 +1,15 @@
 module Lists where
 
-data DList a = DList ([a] -> [a])
+newtype DList a = DList ([a] -> [a])
 
 instance Show a => Show (DList a) where
   show (DList fxs) = show (fxs [])
 
-toList :: DList a -> [a]
-toList (DList fxs) = fxs []
-
-fromList :: [a] -> DList a
-fromList xs = DList (xs ++)
+instance List DList where
+  toList :: DList a -> [a]
+  toList (DList fxs) = fxs []
+  fromList :: [a] -> DList a
+  fromList xs = DList (xs Prelude.++)
 
 concatD :: DList a -> DList a -> DList a
 concatD (DList fxs) (DList fys) = DList (fxs . fys)
@@ -17,12 +17,12 @@ concatD (DList fxs) (DList fys) = DList (fxs . fys)
 concat' :: [[a]] -> [a]
 concat' xss = toList (foldr concatD (DList id) (map fromList xss))
 
-class List a where
-  empty :: List a
-  cons :: a -> List a -> List a
-  snoc :: List a -> a -> List a
-  head :: List a -> a
-  tail :: List a -> List a
-  (++) :: List a -> List a -> List a
-  toList :: List a -> [a]
-  fromList :: [a] -> List a
+class List list where
+  empty :: list a
+  cons :: a -> list a -> list a
+  snoc :: list a -> a -> list a
+  head :: list a -> a
+  tail :: list a -> list a
+  (++) :: list a -> list a -> list a
+  toList :: list a -> [a]
+  fromList :: [a] -> list a
